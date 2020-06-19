bfc6464013bc1aab6db8337953f8b6eaaa773dde
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-06-19 21:06:26 +0530

    Getting Config From Env

--- project/__init__.py
+++ project/__init__.py
@@ -1,10 +1,12 @@
+import os 
 from flask import Flask
 from flask_restx import Resource, Api
 
 app = Flask(__name__)
 
 api = Api(app)
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 
 class Ping(Resource):
