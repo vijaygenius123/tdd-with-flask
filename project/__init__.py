36fa16909c972ea59fc10e32d089969adeb7e129
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-06-19 20:50:35 +0530

    Created Config For Dev, Testing & Prod

--- project/__init__.py
+++ project/__init__.py
@@ -4,6 +4,8 @@ from flask_restx import Resource, Api
 app = Flask(__name__)
 
 api = Api(app)
+app.config.from_object('project.config.DevelopmentConfig')
+
 
 class Ping(Resource):
     def get(self):
