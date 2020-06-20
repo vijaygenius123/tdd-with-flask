490496882c016ba5f7a8c597cd2dbe2087176814
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-06-20 19:17:11 +0530

    Created Base Setup For DB

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,7 @@
 import os 
 from flask import Flask
 from flask_restx import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -8,6 +9,17 @@ api = Api(app)
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
 
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    username = db.Column(db.String(50), nullable= False)
+    email = db.Column(db.String(100), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False) 
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 class Ping(Resource):
     def get(self):
